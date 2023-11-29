d51958cd96e862795fbb15a52dadcd82a3419ad4
linus.lam <linuslam852@gmail.com>
2023-11-29 17:41:23 +0800

    20231129 update

---
+++ ex/src/demo/demoMethod.java
@@ -0,0 +1,36 @@
+package demo;
+
+public class demoMethod {
+  public static void main(String[] args) {
+    String s1 = "abcd";
+    String s2 = "efgh";
+    String s3 = "ab";
+    String s4 = "abcbcdabc";
+    String s5 = "bcd";
+    System.out.println(append(s1,s2));
+    System.out.println("LastChar : " + getLastChar(s2));
+    System.out.println("isStartedWith : " + isStartedWith(s1, s3));
+    System.out.println(remove(s4, s5));
+  }
+  public static String append(String s1, String s2){
+    return s1.concat(s2);
+
+  }
+
+  public static char getLastChar(String s){
+    return s.charAt(s.length()-1);
+  }
+
+  public static boolean isStartedWith(String s, String prefix){
+    for(int i = 0; i < prefix.length(); i++){
+      if(s.charAt(i) != prefix.charAt(i)){
+        return false;
+      }
+    }
+    return true;  
+  }
+
+  public static String remove(String s, String toBeRemoved){
+    return s.replace(toBeRemoved,"");  
+  }
+}
---
+++ ex/src/demo/foreach.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class foreach {
+  public static void main(String[] args) {
+    Integer[] integers = new Integer[]{100,-30,20};
+    for(Integer i : integers){
+      System.out.println("i = " + i);
+    }
+    
+    String str = "abc,def,ijk,xyz";
+    String[] parts = str.split(",");
+    for(String strings : parts){
+      System.out.print(strings);
+      System.out.print(" ");
+    }
+  }
+
+}
